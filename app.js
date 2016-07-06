81ec70f57c06aab62d4201a361b31c488e868473
Hieu Nguyen Duc <isaactesla@gmail.com>
2016-07-06 20:53:51 +0700

    Passing variables to view

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ var express = require('express');
 
 var app = express();
 
+var fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+];
+
 // set up handlebars view engine
 var handlebars = require('express-handlebars')
         .create({defaultLayout: 'main'});
@@ -19,7 +27,9 @@ app.get('/', function (req, res) {
 });
 
 app.get('/about', function (req, res) {
-    res.render('about');
+    var randomFortune =
+    fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', {fortune: randomFortune});
 });
 
 // custom 404 page
