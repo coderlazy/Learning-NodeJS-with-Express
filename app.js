9efd507850aadc61873efc73b226ab66695551eb
Hieu Nguyen Duc <isaactesla@gmail.com>
2016-07-10 04:56:17 +0000

    Block context
    - No need double ..\ back

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var app = express();
 
 // set up handlebars view engine
 var handlebars = require('express-handlebars')
-        .create({defaultLayout: 'main'});
+    .create({
+        defaultLayout: 'main'
+    });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -17,46 +19,64 @@ app.set('ip', process.env.IP || 'localhost');
 app.use(express.static(__dirname + '/public'));
 
 // middleware to detect if test enabled
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     res.locals.showTests = app.get('env') !== 'production' &&
-            req.query.test === '1';
+        req.query.test === '1';
     next();
 });
 
 // set routes
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.render('home');
 });
 
-app.get('/about', function (req, res) {
+app.get('/about', function(req, res) {
     res.render('about', {
         fortune: fortune.getFortune(),
         pageTestScript: '/qa/tests-about.js'
     });
 });
 
-app.get('/tours/hood-river', function (req, res) {
+app.get('/tours/hood-river', function(req, res) {
     res.render('tours/hood-river');
 });
 
-app.get('/tours/request-group-rate', function (req, res) {
+app.get('/tours/request-group-rate', function(req, res) {
     res.render('tours/request-group-rate');
 });
 
+app.get('/tours/payment', function(req, res) {
+    res.render('tours/payment', {
+        currency: {
+            name: 'United States dollars',
+            abbrev: 'USD',
+        },
+        tours: [{
+            name: 'Hood River',
+            price: '$99.95'
+        }, {
+            name: 'Oregon Coast',
+            price: '$159.95'
+        }, ],
+        specialsUrl: '/january-specials',
+        currencies: ['USD', 'GBP', 'BTC'],
+    });
+});
+
 // custom 404 page
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     res.status(404);
     res.render('404');
 });
 
 // custom 500 page
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
     console.error(err.stack);
     res.status(500);
     res.render('500');
 });
 
-app.listen(app.get('port'), function () {
+app.listen(app.get('port'), function() {
     console.log('Express started on http://localhost:' +
-            app.get('port') + '; press Ctrl-C to terminate.');
+        app.get('port') + '; press Ctrl-C to terminate.');
 });
